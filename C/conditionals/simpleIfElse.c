08566acca391312ca0de2cdd8a26dec10377cb89
Pedro Paulo <pedropscontatoprof@gmail.com>
2025-02-11 00:55:28 -0300

    Creating simpleIfElse , and utils txt

---
+++ C/conditionals/simpleIfElse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 5, y =3;
+
+    if(x < y){
+        printf("x é menor que y ");
+    }
+    else if(x > y ){
+        printf ("X é maior que Y");
+
+    }
+    else{
+        printf("X é igual a y " );
+    }
+}

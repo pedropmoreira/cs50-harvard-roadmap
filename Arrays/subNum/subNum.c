11706bde1a452462f5b99a04cea638041bdb8d6c
Pedro Paulo <pedropscontatoprof@gmail.com>
2025-02-18 21:56:30 -0300

    sub num try

---
+++ Arrays/subNum/subNum.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+// exercicio nao dado em sala somente teste 
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Erro \n");
+        return 1;
+    }
+
+    int num = atoi(argv[1]);
+    int resultado = num - 5; 
+
+    printf("%d - 5  = %d\n", num, resultado);
+}
